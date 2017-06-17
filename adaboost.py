Correct stump creation, Fix weight update formula
  - The accuracy for the ensemble was not getting better over time.
  This was due to the fact that stump creation was not being done
  taking instance weights into consideration. Now the ensemble's
  prediction accuracy gets better overtime until a maximum of 98.33%.
  - The weight update formula was using prediction error instead of
  output * hypothesis. This has been corrected.

adaboost.py
@@ -9,6 +9,7 @@ __license__ = "GPL"
 __version__ = "3.0"
 
 import sys
+import time
 import numpy as np
 
 # TODO
@@ -37,18 +38,20 @@ class AdaBoost:
         self.ensemble = []  # Collection of chosen weak learners
         self.alpha = []  # Weight assigned to each weak learner
 
+
     def stump_error(self, stump):
-        predictions = np.ones(self.m)  # 0 if correct, 1 if incorrect
+        predictions = np.zeros(self.m)
+        pred_errors = np.ones(self.m)  # 0 if correct, 1 if incorrect
         a = stump["attribute"]  # Attribute index in training set
         # Loop through instances
         for i in range(self.m):
             value = self.training_set["input"][i][a] 
             output = self.training_set["output"][i]
-            if stump[value] == output:
-                predictions[i] = 0
+            predictions[i] = stump[value]
+            if predictions[i] == output:
+                pred_errors[i] = 0
 
-        error = np.divide(np.sum(np.multiply(self.weights, predictions)), 
-                          np.sum(self.weights))
+        error = np.sum(np.multiply(self.weights, pred_errors))
 
         return error, predictions
 
@@ -73,10 +76,10 @@ class AdaBoost:
             for i in range(self.m):
                 key = self.training_set["input"][i][a]
                 if self.training_set["output"][i] == 1:  # Output is "positive"
-                    ocurrences[key][0] += 1
+                    ocurrences[key][0] += self.weights[i]
                 else:  # Output is "negative"
-                    ocurrences[key][1] += 1
-            
+                    ocurrences[key][1] += self.weights[i]
+
             # Create rule based in most frequent output class per input value
             for key in ocurrences:
                 if ocurrences[key][0] >= ocurrences[key][1]:
@@ -87,11 +90,13 @@ class AdaBoost:
             error, predictions = self.stump_error(stump)
             stump["error"] = error
             stump["predictions"] = predictions
+
             if error < lowest_error:
+                lowest_error = error
                 best_stump = stump
 
         return best_stump
-    
+
 
     def calculate_alpha(self, model):
         error = model["error"]
@@ -101,26 +106,57 @@ class AdaBoost:
 
 
     def update_weights(self, model, alpha):
-        weights_sum = np.sum(self.weights)
+        """
+        Equivalent implementations of weight calculation:
 
-        # Here y_i * h_t(x_i) is equivalent to the model predictions
-        self.weights = np.divide(
-                                 np.multiply(self.weights, 
-                                             np.exp(
-                                                    np.multiply(
-                                                       alpha, 
-                                                       model["predictions"]
-                                                    )
-                                             )
-                                 ), 
-                                 weights_sum
+        for i in range(self.m):
+            self.weights[i] = self.weights[i] * np.exp(-1 * alpha * 
+                      self.training_set["output"][i] * model["predictions"][i])
+
+        self.weights = np.divide(self.weights, np.sum(self.weights))
+        -----------------------------------------------------------------------
+        for i in range(self.m):
+            if model["predictions"][i] != self.training_set["output"][i]:
+                self.weights[i] = np.divide(self.weights[i], 
+                                            2 * model["error"])
+            else:
+                self.weights[i] = np.divide(self.weights[i], 
+                                            2 * (1 - model["error"]))
+        """ 
+
+        self.weights = np.multiply(self.weights, 
+                                   np.exp(-1 * alpha 
+                                     * np.multiply(self.training_set["output"],
+                                                   model["predictions"])
+                                   )
                        )
+        
+        self.weights = np.divide(self.weights, np.sum(self.weights))
+
+
+    def evaluate(self):
+        correct = 0
+        # Loop through instances
+        for i in range(self.m):
+            H = 0
+            for model in range(len(self.ensemble)):
+                H += self.alpha[model] * self.ensemble[model]["predictions"][i]
+            H = np.sign(H)
+
+            if H == self.training_set["output"][i]:
+                correct += 1
+
+        accuracy = (correct / self.m) * 100
+
+        print("The accuracy for the final classifier is: ", accuracy, "%", 
+              sep="")
 
 
     def boost(self, num_iterations):
         for i in range(num_iterations):
             best_model = self.one_rule()
-            print(best_model["error"])
             self.ensemble.append(best_model)
             self.alpha.append(self.calculate_alpha(best_model)) 
+            self.evaluate()
             self.update_weights(best_model, self.alpha[i])
+

main.py
@@ -14,7 +14,7 @@ import adaboost as ab
 
 
 def load_dataset(dataset_name):
-    dataset = np.genfromtxt(dataset_name, dtype = "string", delimiter = ",")
+    dataset = np.genfromtxt(dataset_name, dtype = "str", delimiter = ",")
     return dataset
 
 def output_to_integers(dataset):
@@ -40,7 +40,7 @@ def main():
 
     ada = ab.AdaBoost(dataset)
 
-    ada.boost(100)
+    ada.boost(500)
 
 
 if __name__ == "__main__":